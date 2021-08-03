e3c62fafee59dee78dc263a17569d8689df30151
masiucd <ciszekmarcell@gmail.com>
2021-08-03 13:36:15 +0200

    sub query solution to calc ave

--- instagram-clone/challange.sql
+++ instagram-clone/challange.sql
@@ -19,16 +19,24 @@ ON users.id = photos.user_id
 WHERE photos.id IS NULL;
 -------------------------------------------
 ------------------------------------------- /* We're running a new contest to see who can get the most likes */
-
-
----------------------------------
-SELECT p.id AS photo_id, u.`username`,u.`id` AS user_id, p.`image_url`,COUNT(*) AS most_likes FROM `photos` p
-INNER JOIN likes l 
+--------------------------------- 
+SELECT  p.id     AS photo_id 
+       ,u.`username` 
+       ,u.`id`   AS user_id 
+       ,p.`image_url` 
+       ,COUNT(*) AS most_likes
+FROM `photos` p
+INNER JOIN likes l
 ON l.`photo_id` = p.`id`
 INNER JOIN users u
 ON p.`user_id` = u.`id`
-GROUP BY p.`id`
-ORDER BY most_likes DESC 
-LIMIT 1
-;
-------------------------------------------- 
+GROUP BY  p.`id`
+ORDER BY most_likes DESC
+LIMIT 1 ;
+------------------------------------------- /* Our Investors want to know... How many times does the average user post? */
+--------------------------
+SELECT  (
+SELECT  COUNT(*)
+FROM photos) / (
+SELECT  COUNT(*)
+FROM users) AS average ;
